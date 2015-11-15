38e085303aef53c38c0bc4d28aa1f8b00abcc0b4
haoxiang <samuikwu@gmail.com>
2015-11-15 21:01:26 +0800

    自定义AppView
    减少程序依赖性

---
+++ ApplicationPage/ApplicationPage/AppView.h
@@ -0,0 +1,16 @@
+//
+//  AppView.h
+//  ApplicationPage
+//
+//  Created by 吴皓翔 on 15/11/15.
+//  Copyright © 2015年 Haoxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppView : UIView
+@property (weak, nonatomic) IBOutlet UIImageView *iconView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+@end
---
+++ ApplicationPage/ApplicationPage/AppView.m
@@ -0,0 +1,21 @@
+//
+//  AppView.m
+//  ApplicationPage
+//
+//  Created by 吴皓翔 on 15/11/15.
+//  Copyright © 2015年 Haoxiang. All rights reserved.
+//
+
+#import "AppView.h"
+
+@implementation AppView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
