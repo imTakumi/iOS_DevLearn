338ecdb63526941f93953d432267f670458309fe
haoxiang <samuikwu@gmail.com>
2015-11-16 21:04:52 +0800

    建立猜图程序
    使用storyboard创建上办部分视图

---
+++ 猜图/猜图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  猜图
+//
+//  Created by 吴皓翔 on 15/11/16.
+//  Copyright © 2015年 Haoxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 猜图/猜图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  猜图
+//
+//  Created by 吴皓翔 on 15/11/16.
+//  Copyright © 2015年 Haoxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
