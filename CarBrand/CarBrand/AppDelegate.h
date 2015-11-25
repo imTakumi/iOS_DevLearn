3591dd319e7f9bec47e8b9024cef61a4b05a95e2
haoxiang <samuikwu@gmail.com>
2015-11-25 20:27:49 +0800

    Learn TableView

---
+++ CarBrand/CarBrand/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarBrand
+//
+//  Created by 吴皓翔 on 15/11/25.
+//  Copyright © 2015年 吴皓翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CarBrand/CarBrand/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarBrand
+//
+//  Created by 吴皓翔 on 15/11/25.
+//  Copyright © 2015年 吴皓翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
